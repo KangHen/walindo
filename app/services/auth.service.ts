0449fc65227ca112dcd47d9810aa22b950bc9912
KangHen <hendriarga28@gmail.com>
2025-10-11 10:03:26 +0700

    Add Types

--- app/services/auth.service.ts
+++ app/services/auth.service.ts
@@ -1,31 +1,32 @@
 // services/auth.service.ts
 import { useFetch } from '#app';
+import type { AuthResponse } from '~/types/api/auth';
+import type { User } from '~/types/models/User';
 
 const config = useRuntimeConfig()
 
 const API_URL = config.public.apiUrl;
 
 export const AuthService = {
-  async login(username: string, password: string): Promise<any> {
+  async login(username: string, password: string): Promise<AuthResponse | null> {
     const { data, error } = await useFetch(`${API_URL}/auth/login`, {
       method: 'POST',
       body: { username, password },
     })
 
     if (error.value) throw new Error(error.value?.message || 'Login failed')
-    return data.value
+    return data.value as AuthResponse;
   },
 
-  async me(token: string) {
+  async me(token: string): Promise<User | null> {
     const { data, error } = await useFetch(`${API_URL}/me`, {
       headers: { Authorization: `Bearer ${token}` },
     })
     if (error.value) throw new Error(error.value?.message || 'Fetch user failed')
-    return data.value
+    return data.value as User | null;
   },
 
-  async logout() {
-    const { data } = await useFetch(`${API_URL}/auth/logout`, { method: 'POST' })
-    return data.value
+  async logout(): Promise<void> {
+    await useFetch(`${API_URL}/auth/logout`, { method: 'POST' })
   },
 }
