f3c599206f4cb3b65921a2f62bc61af552697db4
KangHen <hendriarga28@gmail.com>
2025-10-14 21:19:26 +0700

    Provide Routes

--- app/services/auth.service.ts
+++ app/services/auth.service.ts
@@ -2,7 +2,7 @@
 import { useNuxtApp } from "#app";
 import type { AuthResponse } from "~/types/api/auth.response";
 import type { UserResponse } from "~/types/api/user.response";
-import type { User } from "~/types/models/user";
+import { Routes } from "~/types/enums/routes";
 
 const config = useRuntimeConfig();
 const API_URL = config.public.apiUrl;
@@ -13,15 +13,19 @@ export const AuthService = {
     password: string
   ): Promise<AuthResponse | null> {
     const { $api } = useNuxtApp();
-    return (await $api.auth("auth", { username, password })) as AuthResponse;
+    return (await $api.auth(Routes.AUTH, {
+      username,
+      password,
+    })) as AuthResponse;
   },
 
   async me(): Promise<UserResponse | null> {
     const { $api } = useNuxtApp();
-    return (await $api.auth("me")) as UserResponse;
+    return (await $api.auth(Routes.ME)) as UserResponse;
   },
 
   async logout(): Promise<void> {
-    await useFetch(`${API_URL}/auth/logout`, { method: "POST" });
+    const { $api } = useNuxtApp();
+    return await $api.auth(Routes.LOGOUT);
   },
 };
